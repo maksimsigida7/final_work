2da12d36866e658daf9d1ecee5c1b80defce888e
infurgigh <infurgigh@mail.ru>
2022-10-16 18:25:36 +0300

    Фильтрация массива

--- Program.cs
+++ Program.cs
@@ -4,3 +4,27 @@
 // можно ввести с клавиатуры, либо задать на старте выполнения
 // алгоритма. При решении не рекомендуется пользоваться коллекциями,
 // лучше обойтись исключительно массивами.
+
+
+// Печать массива
+void PrintArr(string[] arr, string message = "Массив: ")
+{
+    Console.WriteLine($"{message}[{string.Join(", ", arr)}]");
+}
+
+// Фильтр массива, длина строки меньше 4.
+string[] LessFiltr(string[] words)
+{
+    int index = 0;
+    string[] result = new string[words.Length];
+    foreach (string word in words)
+    {
+        if (word.Length < 4)
+        {
+            result[index] = word;
+            index++;
+        }
+    }
+    Array.Resize(ref result, index);
+    return result;
+}
